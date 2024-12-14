155cf73f61b27752ca2a7a8f08aa22beb4168a79
diwakar8686 <diw2akaraubr@gmail.com>
2024-12-14 23:24:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function processProducts(products) {
+    const productNames = products.map(product => product.name);
+    productNames.forEach(name => {
+      const product = products.find(product => product.name === name);
+      if (product.price > 50) {
+        console.log(`${name} is above $50`);
+      } else {
+        console.log(`${name} is below $50`);
+      }
+    });
+  }
+  const products = [
+    { name: "Laptop", price: 1000 },
+    { name: "Mouse", price: 20 }
+  ];
+  
+  processProducts(products);
+  
